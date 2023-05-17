6fe6799d55f0516ab83edcf72283e4c48ab862b7
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-05-17 23:05:48 +0300

    Update deleteTransaction.js

--- controllers/transactions/deleteTransaction.js
+++ controllers/transactions/deleteTransaction.js
@@ -1,12 +1,26 @@
 const { Transaction } = require("../../models");
+const { User } = require("../../models");
 
 const deleteTransaction = async (req, res) => {
+  const { _id: owner } = req.user;
   const { id } = req.params;
 
   try {
     const deletedTransaction = await Transaction.findByIdAndRemove({
       _id: id,
     });
+
+    const finalSum =
+      deletedTransaction.type === "income"
+        ? -deletedTransaction.sum
+        : deletedTransaction.sum;
+
+    await User.findByIdAndUpdate(
+      { _id: owner },
+      { $inc: { balance: finalSum } },
+      { new: true }
+    );
+
     res.json(deletedTransaction);
   } catch (error) {
     console.log(error.message);
